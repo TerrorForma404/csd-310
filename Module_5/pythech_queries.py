ae03c245483e9d25a366182ef03caba940097bc5
TerrorForma404 <79122754+TerrorForma404@users.noreply.github.com>
2021-11-21 19:26:09 -0600

    Update pythech_queries.py

--- Module_5/pythech_queries.py
+++ Module_5/pythech_queries.py
@@ -7,6 +7,6 @@ student_list = students.find({})
 print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 for doc in student_list:
     print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
-one = students.find_one({"student_id": "0001"})
+one = students.find_one({"student_id": "1007"})
 print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
 print("  Student ID: " + one["student_id"] + "\n  First Name: " + one["first_name"] + "\n  Last Name: " + one["last_name"] + "\n")
